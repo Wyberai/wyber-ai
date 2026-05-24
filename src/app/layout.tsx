d72f23585b3b71bf7234aad04fd5b06ee1201199
sumeet-jpg <sumeet-jpg@users.noreply.github.com>
2026-05-25 01:28:44 +0530

    feat: add Tawk.to live chat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {children}
         </ThemeProvider>
 
+      {/* Tawk.to Live Chat */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/6a1357c68673aa1c3e767404/1jpdoto52';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
